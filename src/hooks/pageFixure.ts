f2dbce8e6557bc067cf019d763b950f0e9027ee2
pradeep jadhav <158508234+pradeepjadhav3@users.noreply.github.com>
2026-07-03 01:26:53 +0530

    committed

--- src/hooks/pageFixure.ts
+++ src/hooks/pageFixure.ts
@@ -1,8 +1,10 @@
-import { Page } from "@playwright/test";
+import { APIRequestContext, Page } from "@playwright/test";
+import { Logger } from "winston";
+import { LoginPage } from "../pages/loginPage";
 
-
-export const pageFixture = {
-
-    page: undefined as unknown as Page
-
+export const fixture = {
+    page: undefined as unknown as Page,
+    request: undefined as unknown as APIRequestContext,
+    logger: undefined as unknown as Logger,
+    loginPage: undefined as unknown as LoginPage,
 };
